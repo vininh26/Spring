68b5baee367e675603d0363a6afde0bb385d41ac
vi ninh <donkey.n1160@gmail.com>
2022-07-11 11:04:23 +0700

    update

---
+++ src/main/java/com/example/SpringPosgres/SpringPosgresApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringPosgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPosgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPosgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringPosgres/SpringPosgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringPosgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPosgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
